ecbd0c5fd6dda3b0b5b7c46f2f9e299edea03544
DeJuan <dejuanvann@gmail.com>
2022-07-06 08:09:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// let total = 0.0;
+
+// document.getElementById("cola").addEventListener("click", function () {
+//   total += 2.0;
+//   document.getElementById("total").innerHTML = total.toFixed(2);
+// });
+
+// document.getElementById("peanuts").addEventListener("click", function () {
+//   total += 3.0;
+//   document.getElementById("total").innerHTML = total.toFixed(2);
+// });
+
+// document.getElementById("chocolate").addEventListener("click", function () {
+//   total += 4.0;
+//   document.getElementById("total").innerHTML = total.toFixed(2);
+// });
+
+// document.getElementById("gummies").addEventListener("click", function () {
+//   total += 5.0;
+//   document.getElementById("total").innerHTML = total.toFixed(2);
+// });
+
+//answer
+// let sum = 0;
+// const totalParagraph = document.querySelector(".total");
+// const cola = document.querySelector(".cola")
+// const peanuts = document.querySelector(".peanuts")
+// const chocolate = document.querySelector(".chocolate")
+// const gummies = document.querySelector(".gummies")
+const changeForm = document.querySelector
+
+
+
+
+
+
+
+const moneyForm = document.querySelector("form");
+
+moneyForm.addEventListener("submit", (e) =>) {
+    e.prventDefault();
+    const amount = document.querySelector("#amount").value 
+    const type = document.querySelector("#type").value
+    for(let i = 0; i <amount; i++){
+        const newCoin = document.createElement("li");
+    }
+}
+
+
+
